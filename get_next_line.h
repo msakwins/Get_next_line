6f38ad68ace156dd5c93b3449d719269dfde81e5
Marion SAKWINSKI <msakwins@e1r8p4.42.fr>
2018-05-07 15:54:02 +0200

    Get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msakwins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/30 14:25:03 by msakwins          #+#    #+#             */
+/*   Updated: 2017/08/29 11:20:29 by msakwins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <fcntl.h>
+
+# define MAX_FD 10420
+# define BUFF_SIZE 8
+
+int	get_next_line(int const fd, char **line);
+
+#endif
